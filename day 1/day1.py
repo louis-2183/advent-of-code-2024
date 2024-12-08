7b204edc29004c2ac797082b8f086547c9d66b8e
Louis <147085290+louis-2183@users.noreply.github.com>
2024-12-08 17:31:25 +0000

    Update day1.py

--- day 1/day1.py
+++ day 1/day1.py
@@ -1,31 +1,23 @@
-import numpy as np
 import operator
 from functools import lru_cache
 import time
 
+start_time = time.time()
+
 ######################## Part 1 ############################
 
-# Open the file
-with open('input.txt', 'r') as file:
-    # Read each line and take out the separators
-    lines = file.read().replace('\n','   ').split('   ')[:-1]
-    
-lines_int = [int(x) for x in lines]
+data = list(map(int,open('input.txt').read()[:-1].replace('\n','   ').split('   ')))
 
 # Get columns from odd/even items
-left_list = sorted(lines_int[::2])
-right_list = sorted(lines_int[1::2])
+left_list,right_list = sorted(data[::2]), sorted(data[1::2])
 
 # Distance between everything - subtract, get abs value
-distances = list(map(operator.sub,left_list,right_list))
-abs_distances = [abs(x) for x in distances]
+distances = list(map(abs,map(operator.sub,left_list,right_list)))
 
 # Part 1 answer
-print(np.sum(abs_distances))
+print(sum(distances))
 
 ######################## Part 2 ############################
-
-start_time = time.time()
 
 # For the right (even) set there are ~400 duplicated numbers
 # If we want our cache results to speed it up, we have to calculate going from the right to left list - this gives the same result
@@ -38,9 +30,10 @@ def get_score(item):
 scores = [get_score(x) for x in right_list]
 
 # Part 2 answer
-print(np.sum(scores))
+print(sum(scores))
+
+###########################################################
 
 # Adding the cache saves a bit of time
 end_time = time.time()
 print(end_time-start_time)
-
